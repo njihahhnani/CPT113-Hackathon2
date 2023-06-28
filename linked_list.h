224e492a7461fedf6e870c1df5b0a4b4a6ad7cb6
njihahhnani <117801046+njihahhnani@users.noreply.github.com>
2023-06-28 13:47:04 +0800

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -9,51 +9,51 @@ template <class T>
 class LinkedList
 {
 public:
-    LinkedList();
-    ~LinkedList();
+    LinkedList(); // Constructor
+    ~LinkedList(); // Destructor
 
-    void add(const T value);
-    void addFront(const T value);
-    void remove();
-    void removeFront();
-    void removeAt(const int index);
+    void add(const T value); // Add a value to the end of the list
+    void addFront(const T value); // Add a value to the front of the list
+    void remove(); // Remove the last value from the list
+    void removeFront(); // Remove the first value from the list
+    void removeAt(const int index); // Remove a value at the specified index
 
-    void stackPush(const T value);
-    void stackPop();
+    void stackPush(const T value); // Push a value onto the stack (add to the front)
+    void stackPop(); // Pop a value from the stack (remove from the front)
 
-    void clear();
-    void print();
+    void clear(); // Clear the entire list
+    void print(); // Print the values in the list
 
-    bool contains(const T value) const;
-    bool isEmpty() const;
+    bool contains(const T value) const; // Check if the list contains a given value
+    bool isEmpty() const; // Check if the list is empty
 
-    int getSize() const;
+    int getSize() const; // Get the size of the list
 
-    T at(const int index) const;
+    T at(const int index) const; // Get the value at the specified index
 
 protected:
-    struct Node
+    struct Node // Definition of a Node
     {
-        T *value;
-        Node *next;
+        T *value; // Pointer to the stored value of type T
+        Node *next; // Pointer to the next Node
     };
 
-    Node *head;
+    Node *head; // Pointer to the head (first Node) of the list
 };
 
 template <class T>
 LinkedList<T>::LinkedList()
 {
-    head = new Node();
-    head->value = NULL;
-    head->next = NULL;
+    head = new Node(); // Create a new head Node
+    head->value = NULL; // Set the value of the head Node to NULL
+    head->next = NULL; // Set the next pointer of the head Node to NULL
 }
 
 template <class T>
 LinkedList<T>::~LinkedList()
 {
-    clear();
-    delete head;
+    clear(); // Call the clear function to deallocate memory
+    delete head; // Delete the head Node
 }
 
 template <class T>
@@ -65,17 +65,17 @@ void LinkedList<T>::add(const T value)
 
         while (current->next != NULL)
         {
-            current = current->next;
+            current = current->next; // Traverse to the last Node
         }
 
-        current->next = new Node();
-        current->next->value = new T(value);
-        current->next->next = NULL;
+        current->next = new Node(); // Create a new Node
+        current->next->value = new T(value); // Set the value of the new Node
+        current->next->next = NULL; // Set the next pointer of the new Node to NULL
     }
     else
     {
-        head->value = new T(value);
-        head->next = NULL;
+        head->value = new T(value); // Set the value of the head Node
+        head->next = NULL; // Set the next pointer of the head Node to NULL
     }
 }
 
@@ -84,29 +84,29 @@ void LinkedList<T>::addFront(const T value)
 {
     if (head->value != NULL)
     {
-        Node *node = new Node();
+        Node *node = new Node(); // Create a new Node
 
-        node->value = new T(value);
-        node->next = head;
-        head = node;
+        node->value = new T(value); // Set the value of the new Node
+        node->next = head; // Set the next pointer of the new Node to the current head Node
+        head = node; // Set the new Node as the head Node
     }
     else
     {
-        head->value = new T(value);
-        head->next = NULL;
+        head->value = new T(value); // Set the value of the head Node
+        head->next = NULL; // Set the next pointer of the head Node to NULL
     }
 }
 
 template <class T>
 void LinkedList<T>::remove()
 {
-    removeAt(getSize() - 1);
+    removeAt(getSize() - 1); // Remove the last value by calling removeAt with the index of the last value
 }
 
 template <class T>
 void LinkedList<T>::removeFront()
 {
-    removeAt(0);
+    removeAt(0); // Remove the first value by calling removeAt with index 0
 }
 
 template <class T>
@@ -121,17 +121,15 @@ void LinkedList<T>::removeAt(const int index)
 
         while (current != NULL)
         {
-
             if (currentIndex == index)
             {
-
                 if (previous != NULL)
-                    previous->next = next;
+                    previous->next = next; // Adjust the next pointer of the previous Node
                 else
-                    head = current->next;
+                    head = current->next; // Update the head Node if the first Node is removed
 
-                delete current->value;
-                delete current;
+                delete current->value; // Delete the value of the current Node
+                delete current; // Delete the current Node
                 break;
             }
             else
@@ -148,13 +146,13 @@ void LinkedList<T>::removeAt(const int index)
 template <class T>
 void LinkedList<T>::stackPush(const T value)
 {
-    addFront(value);
+    addFront(value); // Push a value onto the stack by calling addFront
 }
 
 template <class T>
 void LinkedList<T>::stackPop()
 {
-    removeFront();
+    removeFront(); // Pop a value from the stack by calling removeFront
 }
 
 template <class T>
@@ -167,15 +165,15 @@ void LinkedList<T>::clear()
 
         while (current != NULL)
         {
-            next = current->next;
-            delete current->value;
-            delete current;
-            current = next;
+            next = current->next; // Store the next pointer before deleting the current Node
+            delete current->value; // Delete the value of the current Node
+            delete current; // Delete the current Node
+            current = next; // Move to the next Node
         }
 
-        head = new Node();
-        head->value = NULL;
-        head->next = NULL;
+        head = new Node(); // Create a new head Node
+        head->value = NULL; // Set the value of the head Node to NULL
+        head->next = NULL; // Set the next pointer of the head Node to NULL
     }
 }
 
@@ -187,8 +185,8 @@ void LinkedList<T>::print()
 
     while (current != NULL)
     {
-        cout << *current->value << " ";
-        current = current->next;
+        cout << *current->value << " "; // Print the value of the current Node
+        current = current->next; // Move to the next Node
     }
 
     cout << endl;
@@ -201,11 +199,10 @@ bool LinkedList<T>::contains(const T value) const
 
     while (current != NULL)
     {
-
-        if (*current->value == value)
+        if (*current->value == value) // Compare the value of the current Node with the given value
             return true;
         else
-            current = current->next;
+            current = current->next; // Move to the next Node
     }
 
     return false;
@@ -228,8 +225,8 @@ int LinkedList<T>::getSize() const
 
     while (current != NULL)
     {
-        size++;
-        current = current->next;
+        size++; // Increment the size
+        current = current->next; // Move to the next Node
     }
 
     return size;
@@ -244,14 +241,15 @@ T LinkedList<T>::at(const int index) const
     while (current != NULL)
     {
         if (currentIndex == index)
-            return *current->value;
+            return *current->value; // Return the value of the current Node at the specified index
         else
             currentIndex++;
 
-        current = current->next;
+        current = current->next; // Move to the next Node
     }
 
     return T();
 }
 
 #endif
+
